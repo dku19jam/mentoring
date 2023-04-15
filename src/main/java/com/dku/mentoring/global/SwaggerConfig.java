1f9158469c1561782cd6332881d71c6516736576
dku19jam <70689205+dku19jam@users.noreply.github.com>
2023-04-15 16:16:13 +0900

    chore: swagger에서 jwt 토큰 정보를 담아서 요청 할 수 있도록 변경

--- src/main/java/com/dku/mentoring/global/SwaggerConfig.java
+++ src/main/java/com/dku/mentoring/global/SwaggerConfig.java
@@ -1,28 +1,46 @@
 package com.dku.mentoring.global;
 
-import com.dku.mentoring.global.auth.JwtProvider;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(
-                title = "SW융합대학 멘토링 서버 API",
-                version = SwaggerConfig.API_VERSION,
-                description = "RESTFUL API 제공"
-        ),
-        servers = {
-                @Server(url = "/", description = "로컬 서버")
-        }
-
-)
-
 public class SwaggerConfig {
-    public static final String API_VERSION = "v1.0.0";
+    private static final String REFERENCE = "Authorization 헤더 값";
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any()).build().securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(securityScheme()));
+    }
 
+    private SecurityContext securityContext() {
+        return springfox.documentation.spi.service.contexts.SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .operationSelector(operationContext -> true)
+                .build();
+    }
+
+    private List defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = new AuthorizationScope("global", "accessEverything");
+        return List.of(new SecurityReference(REFERENCE, authorizationScopes));
+    }
+
+    private ApiKey securityScheme() {
+        String targetHeader = "Authorization";
+        return new ApiKey(REFERENCE, targetHeader, "header");
+
+    }
 }
