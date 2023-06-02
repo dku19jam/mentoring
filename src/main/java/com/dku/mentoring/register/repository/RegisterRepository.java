b4c5f810ba83d31eeb2684615e724805e0f74f23
gutanbug53 <gutanbug53@gmail.com>
2023-06-02 15:48:59 +0900

    fix:전체 등록 글 수정

--- src/main/java/com/dku/mentoring/register/repository/RegisterRepository.java
+++ src/main/java/com/dku/mentoring/register/repository/RegisterRepository.java
@@ -10,6 +10,6 @@ public interface RegisterRepository extends JpaRepository<Register, Long>{
 
     Page<Register> findByUserId(Long userId, Pageable pageable);
 
-    @Query("select r from Register r order by r.createdAt DESC")
+    @Query("select r from Register r where r.status = 'PROGRESS' order by r.createdAt DESC")
     Page<Register> findAllRegisters(Pageable pageable);
 }
